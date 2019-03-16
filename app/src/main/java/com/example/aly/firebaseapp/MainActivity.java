ab72f376b456fbd00d0cad54dcffb6936355bf21
Layyim <layyim@yahoo.com.sg>
2019-03-16 15:47:20 +0800

    Initial commit

---
+++ app/src/main/java/com/example/aly/firebaseapp/MainActivity.java
@@ -0,0 +1,138 @@
+package com.example.aly.firebaseapp;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity
+{
+    private EditText edtEmail, edtUsername, edtPassword;
+    private Button btnSignUp, btnSignIn;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtEmail = findViewById(R.id.edtEmail);
+        edtUsername = findViewById(R.id.edtUsername);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnSignIn = findViewById(R.id.btnSignIn);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnSignUp.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                signUp();
+            }
+        });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                signIn();
+            }
+        });
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null)
+        {
+            transitionToSocialMediaActivity();
+        }
+    }
+
+    private void signUp()
+    {
+        mAuth.createUserWithEmailAndPassword(edtEmail.getText().toString(), edtPassword.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if (task.isSuccessful())
+                        {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(MainActivity.this, "Sign Up Successful",
+                                    Toast.LENGTH_SHORT).show();
+
+                            FirebaseDatabase.getInstance().getReference().child("my_users")
+                                    .child(task.getResult().getUser().getUid()).child("username")
+                                    .setValue(edtUsername.getText().toString());
+
+                            transitionToSocialMediaActivity();
+                        }
+
+                        else
+                        {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Sign Up Failed",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+                });
+    }
+
+    private void signIn()
+    {
+        mAuth.signInWithEmailAndPassword(edtEmail.getText().toString(), edtPassword.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
+                {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if (task.isSuccessful())
+                        {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(MainActivity.this, "Sign In Successful",
+                                    Toast.LENGTH_SHORT).show();
+
+                            transitionToSocialMediaActivity();
+                        }
+
+                        else
+                        {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Sign In Failed",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    }
+
+    private void transitionToSocialMediaActivity()
+    {
+        Intent intent = new Intent(this, SocialMediaActivity.class);
+        startActivity(intent);
+    }
+}
